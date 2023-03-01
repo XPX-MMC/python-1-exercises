64d5ae2f8df02b952331ef51654252b8fdbbd21e
Martin Burolla <mburolla@xpanxion.com>
2023-03-01 16:03:50 -0500

    update

--- ReadMe.md
+++ ReadMe.md
@@ -2,4 +2,7 @@
 
 # Getting Started
 - Clone this repo
-
+- Open directory with IntelliJ
+- Create Python configuration
+- Update [main.py](main.py) with your student id
+- Submit a PR for each exercise
--- students/stu0/assignments.py
+++ students/stu0/assignments.py
@@ -9,3 +9,31 @@ def ex2():
 def ex3():
     print("ex 3")
 
+
+def ex4():
+    print("ex 4")
+
+
+def ex5():
+    print("ex 5")
+
+
+def ex6():
+    print("ex 6")
+
+
+def ex7():
+    print("ex 7")
+
+
+def ex8():
+    print("ex 8")
+
+
+def ex9():
+    print("ex 9")
+
+
+def ex10():
+    print("ex 10")
+
